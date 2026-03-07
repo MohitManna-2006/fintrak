c58f3db5f86575ae1daad3f5addb2c66949f60ab
Mohit <mannamohit542@gmail.com>
2026-03-07 13:43:12 -0500

    fix2

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,19 +1,6 @@
 import path from "path";
 import { defineConfig } from "prisma/config";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
-
-const adapter = new PrismaPg(pool);
 
 export default defineConfig({
-  earlyAccess: true,
   schema: path.join("prisma", "schema.prisma"),
-  migrate: {
-    adapter,
-  },
 });
